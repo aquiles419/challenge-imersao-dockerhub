5296fdf81b20ea44077ad2916ec24ea206f0cb0c
Aquiles <wesley419@gmail.com>
2023-06-21 15:14:01 -0300

    fix: refactor files

--- hello.go
+++ hello.go
@@ -1,16 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
+import "fmt"
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
-	})
-
 	fmt.Println("Full Cycle Rocks!!")
-	fmt.Println("🚀 Server listening on port 8080")
-	http.ListenAndServe(":8080", nil)
 }
